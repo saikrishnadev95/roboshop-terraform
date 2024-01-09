7daafa0f169ae2161cbd1f632f7602780dd7cc56
saikrishnadev95 <saikrishnatsk35@gmail.com>
2024-01-09 10:49:40 +0530

    terraform changes

--- servers.tf
+++ servers.tf
@@ -1,12 +1,14 @@
 data "aws_ami" "centos" {
+  owners           = ["973714476881"]
   executable_users = ["self"]
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
-  owners           = ["973714476881"]
 }
-output = "aws_ami" {
-   value = data.aws_ami.centos.image_id
+
+output "ami" {
+  value = data.aws_ami.centos.image_id
 }
+
 resource "aws_instance" "frontend" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
