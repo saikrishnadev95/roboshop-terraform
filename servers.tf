a0af3eafc27c5fd7b32167df39b13e3cdaaf66d1
saikrishnadev95 <saikrishnatsk35@gmail.com>
2024-01-10 10:08:26 +0530

    terraform changes

--- servers.tf
+++ servers.tf
@@ -4,10 +4,6 @@ data "aws_ami" "centos" {
   name_regex       = "Centos-8-DevOps-Practice"
 }
 
-output "ami" {
-  value = data.aws_ami.centos.image_id
-}
-
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -17,17 +13,29 @@ resource "aws_instance" "frontend" {
   }
 }
 
-output "frontend" {
-  value = aws_instance.frontend.public_ip
+resource "aws_route53_record" "frontend" {
+  zone_id = Z07043833GN76DBYXNYXF
+  name    = "frontend.saikrishna.cloud"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.frontend.private_ip]
 }
 
-resource "aws_instance" "backend" {
+resource "aws_instance" "mongodb" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
 
   tags = {
-    Name = "backend"
+    Name = "mongodb"
   }
+}
+
+resource "aws_route53_record" "mongodb" {
+  zone_id = Z07043833GN76DBYXNYXF
+  name    = "mongodb.saikrishna.cloud"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mongodb.private_ip]
 }
 
 resource "aws_instance" "catalogue" {
@@ -39,6 +47,14 @@ resource "aws_instance" "catalogue" {
   }
 }
 
+resource "aws_route53_record" "catalogue" {
+  zone_id = Z07043833GN76DBYXNYXF
+  name    = "catalogue.saikrishna.cloud"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.catalogue.private_ip]
+}
+
 resource "aws_instance" "user" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -46,6 +62,14 @@ resource "aws_instance" "user" {
   tags = {
     Name = "user"
   }
+}
+
+resource "aws_route53_record" "user" {
+  zone_id = Z07043833GN76DBYXNYXF
+  name    = "user.saikrishna.cloud"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.user.private_ip]
 }
 
 resource "aws_instance" "redis" {
@@ -57,6 +81,14 @@ resource "aws_instance" "redis" {
   }
 }
 
+resource "aws_route53_record" "redis" {
+  zone_id = Z07043833GN76DBYXNYXF
+  name    = "redis.saikrishna.cloud"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.redis.private_ip]
+}
+
 resource "aws_instance" "cart" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -64,6 +96,14 @@ resource "aws_instance" "cart" {
   tags = {
     Name = "cart"
   }
+}
+
+resource "aws_route53_record" "cart" {
+  zone_id = Z07043833GN76DBYXNYXF
+  name    = "cart.saikrishna.cloud"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.cart.private_ip]
 }
 
 resource "aws_instance" "mysql" {
@@ -75,6 +115,14 @@ resource "aws_instance" "mysql" {
   }
 }
 
+resource "aws_route53_record" "mysql" {
+  zone_id = Z07043833GN76DBYXNYXF
+  name    = "mysql.saikrishna.cloud"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mysql.private_ip]
+}
+
 resource "aws_instance" "shipping" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -82,6 +130,14 @@ resource "aws_instance" "shipping" {
   tags = {
     Name = "shipping"
   }
+}
+
+resource "aws_route53_record" "shipping" {
+  zone_id = Z07043833GN76DBYXNYXF
+  name    = "shipping.saikrishna.cloud"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.shipping.private_ip]
 }
 
 resource "aws_instance" "rabbitmq" {
@@ -93,6 +149,14 @@ resource "aws_instance" "rabbitmq" {
   }
 }
 
+resource "aws_route53_record" "rabbitmt" {
+  zone_id = Z07043833GN76DBYXNYXF
+  name    = "rabbitmq.saikrishna.cloud"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.rabbitmq.private_ip]
+}
+
 resource "aws_instance" "payment" {
   ami           = data.aws_ami.centos.image_id
   instance_type = "t3.micro"
@@ -100,4 +164,12 @@ resource "aws_instance" "payment" {
   tags = {
     Name = "payment"
   }
+}
+
+resource "aws_route53_record" "payment" {
+  zone_id = Z07043833GN76DBYXNYXF
+  name    = "frontend.saikrishna.cloud"
+  type    = "A"
+  ttl     = 300
+  records = [aws_instance.mongodb.private_ip]
 }
