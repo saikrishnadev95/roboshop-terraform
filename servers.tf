6011a5959cd2a87c05676993fc0f54f7600d2774
saikrishnadev95 <saikrishnatsk35@gmail.com>
2024-01-15 15:25:42 +0530

    terraform changes

--- servers.tf
+++ servers.tf
@@ -4,9 +4,18 @@ data "aws_ami" "centos" {
   name_regex       = "Centos-8-DevOps-Practice"
 }
 
+data "aws_security_group" "allow-all" {
+  name = "allow-all"
+}
+
+variable "instance_type" {
+  default = "t3.small"
+}
+
 resource "aws_instance" "frontend" {
   ami           = data.aws_ami.centos.image_id
-  instance_type = "t3.micro"
+  instance_type = var.instance_type
+  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
 
   tags = {
     Name = "frontend"
